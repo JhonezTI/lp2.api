422e51611ba5671e0c951be2f29d3c2149e7fc1a
JhonezTI <jonas.sales.reboucas08@aluno.ifce.edu.br>
2021-09-13 16:36:20 -0300

    Finalizacao do Spring

---
+++ src/main/java/br/edu/ifce/lp2/ecommercerbook/ECommercerBookApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2.ecommercerbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercerBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercerBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifce/lp2/ecommercerbook/ECommercerBookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2.ecommercerbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercerBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
